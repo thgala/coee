4d24ff8498b1a14dee2273caa0d4e7d32dfc78f8
Konstantin Smirnov <konstantin@Konstantins-iMac.local>
2018-03-10 19:42:47 +0100

    Home page kind of done

--- src/component/sideBar/index.js
+++ src/component/sideBar/index.js
@@ -8,7 +8,6 @@ import Text from '../text'
 const mock = {
   text_1: `Video trailer`,
   text_2: `The college of extraordinary experiences`,
-  text_3: `1 | 14`,
 }
 
 const bemConfig = {
@@ -16,41 +15,66 @@ const bemConfig = {
 }
 
 class SideBar extends React.Component {
-  componentDidMount() {
-    this.calculateRefStyles()
+
+  state = {
+    videoTextInnerWidth: null,
+    videoTextInnerHeight: null,
+    titleInnerWidth: null,
+    titleInnerHeight: null,
   }
 
-  calculateRefStyles() {
-    const sizes = {
-      videoTextInnerWidth: this.videoTextInner.offsetWidth,
-      videoTextInnerHeight: this.videoTextInner.offsetHeight,
-      titleInnerWidth: this.titleInner.offsetWidth,
-      titleInnerHeight: this.titleInner.offsetHeight,
-    }
-    
-    this.videoTextInner.style.width = `${sizes.videoTextInnerHeight}px`
-    this.videoTextInner.style.height = `${sizes.videoTextInnerWidth}px`
+  componentDidMount() {
+    this.calculateRefStyles()
+    window.addEventListener('resize', this.calculateRefStyles)
+  }
 
-    this.titleInner.style.width = `${sizes.titleInnerHeight}px`
-    this.titleInner.style.height = `${sizes.titleInnerWidth}px`
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.calculateRefStyles);
+  }
+
+  calculateRefStyles = () => {
+    this.setState({
+      videoTextInnerWidth: `${this.videoTextInner.offsetWidth}px`,
+      videoTextInnerHeight: `${this.videoTextInner.offsetHeight}px`,
+      titleInnerWidth: `${this.titleInner.offsetWidth}px`,
+      titleInnerHeight: `${this.titleInner.offsetHeight}px`,
+    })
   }
 
   render () {
+    const {
+      videoTextInnerWidth,
+      videoTextInnerHeight,
+      titleInnerWidth,
+      titleInnerHeight,
+    } = this.state
+
     return (
       <div className={this.block()}>
         <div className={this.element('videoIcon')} />
-        <div className={this.element('videoText')}>
-          <div className={this.element('videoTextInner rotator')} ref={node => this.videoTextInner = node}>
+        <div className={this.element('videoText')} ref={node => this.videoTextInner = node}>
+          <div
+            className={this.element('videoTextInner rotator')}
+            style={{
+              width: videoTextInnerHeight,
+              height: videoTextInnerWidth,
+            }}>
             <Text value={mock.text_1} small/>
           </div>
         </div>
-        <div className={this.element('title')}>
-          <div className={this.element('titleInner rotator')}  ref={node => this.titleInner = node}>
+        <div className={this.element('title')} ref={node => this.titleInner = node}>
+          <div
+            className={this.element('titleInner rotator')}
+            style={{
+              width: titleInnerHeight,
+              height: titleInnerWidth,
+            }}>
             <Text value={mock.text_2} small />
           </div>
         </div>
         <div className={this.element('num')}>
-          <Text value={mock.text_3} />
+          <Text value='1' small />
+          <Text value='14' small />
         </div>
       </div>
     )
